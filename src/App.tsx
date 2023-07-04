7fe7efcab69ec626f2b9e513565960ce09c21216
Dipto Karmakar <diptokmk47@gmail.com>
2023-07-05 02:18:46 +0600

    Update gap

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import QRImg from './assets/image-qr-code.png'
 function App() {
   return (
     <div className="bg-light_gray h-screen flex items-center justify-center">
-      <div className="container w-72">
+      <div className="container w-[300px]">
         <Card
           imgSrc={QRImg}
           title="Improve your front-end skills by building projects"
